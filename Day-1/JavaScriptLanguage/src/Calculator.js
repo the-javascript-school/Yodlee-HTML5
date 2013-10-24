1c423d66fc983f9b1a7669f43caf56e035d51738
the-javascript-school <tkmagesh@craftsmanfactory.com>
2013-10-24 16:57:28 -0700

    JavaScript Language Study - 3

--- Day-1/JavaScriptLanguage/src/Calculator.js
+++ Day-1/JavaScriptLanguage/src/Calculator.js
@@ -1,12 +1,9 @@
 function add(){
 	function parseArg(n){
-		if (typeof n === "number") return n;
-		if (typeof n === "string" && !isNaN(n)) return +(n);
+		if (!isNaN(n)) return +(n);
 		if (typeof n === "function") return parseArg(n());
-		if (!!n.length) return add.apply(this,n);
+		if (!!n && !!n.length) return add.apply(this,n);
 		return 0;
 	}
-	var result = 0;
-	for(var i=0;i<arguments.length;i++) result += parseArg(arguments[i]);
-	return result;
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add.apply(this,[].splice.call(arguments,1));
 }
