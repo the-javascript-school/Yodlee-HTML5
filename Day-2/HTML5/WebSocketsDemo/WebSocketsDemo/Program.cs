54c9643b876f1f5ad22c4211545d814185194448
the-javascript-school <tkmagesh@craftsmanfactory.com>
2013-10-25 17:49:40 -0700

    Web Sockets Demo

---
+++ Day-2/HTML5/WebSocketsDemo/WebSocketsDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebSocketsDemo
+{
+    class Program
+    {
+        static IWebSocketServer server = new WebSocketServer("ws://localhost:9090/SocketServer");
+        
+        static void Main(string[] args)
+        {
+            List<IWebSocketConnection> conns = new List<IWebSocketConnection>();
+            server.Start(con => {
+                con.OnOpen += () =>
+                {
+                    Console.WriteLine("A new Connection is established");
+                    conns.Add(con);
+                };
+                con.OnMessage += (msg) => {
+                    Console.WriteLine("Message received - {0}",msg);
+                    foreach (var cn in conns) {
+                        if (cn != con)
+                            cn.Send(msg);
+                    }
+                };
+                con.OnClose += () =>
+                {
+                    Console.WriteLine("An existing connection is closed");
+                    conns.Remove(con);
+                };
+            });
+            string input = string.Empty;
+            while((input = Console.ReadLine()).ToUpper() != "EXIT" )
+                foreach (var cn in conns)
+                {
+                    cn.Send(input);
+                }
+
+        }
+
+    }
+}
